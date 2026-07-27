91f412f514e8e73ecfc9966b81f1add421a0e2c4
Timothy Tan <timothytan94@gmail.com>
2026-07-27 18:35:57 +0800

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        int left =0;
+        int right =1;
+        while(right<prices.length){
+            if(prices[right]>prices[left]){
+                int profit = prices[right]-prices[left];
+                maxProfit = Math.max(profit,maxProfit);
+            }else{
+                left=right;
+            }
+            right++;
+            
+        }
+        return maxProfit;
+    }
+}
