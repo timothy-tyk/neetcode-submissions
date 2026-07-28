6cda7a02cb6a232f13aec202d6e8fe8b6ac8f901
Timothy Tan <timothytan94@gmail.com>
2026-07-28 09:42:27 +0800

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int maxArea = 0;
+        int left = 0;
+        int right=heights.length-1;
+
+        while(left<right){
+            int area = (right-left)* Math.min(heights[left], heights[right]);
+            maxArea = Math.max(area, maxArea);
+            if(heights[left]>heights[right]) {
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+
+        return maxArea;
+    }
+}
+
+/**
+ * 2 pointer approach
+ * 
+ * max=0;
+ * calc area between left and right pointer
+ *  - area = (right-left)* min(height[right],height[left])
+ *  - if area>max, max = area
+ *  - how to proceed further?
+ *      - if height[left] > height[right], right++
+ *      - if height[left] < height[right], left--
+ *      - keep moving out the lower boundary, as there is a chance the next boundary has more height
+ */
